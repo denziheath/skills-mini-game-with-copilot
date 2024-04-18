39c0dddd90372ce37999545ff3846ab75a27aabf
denziheath <118496561+denziheath@users.noreply.github.com>
2024-04-18 21:49:43 +0000

    added rock paper scissors game

--- app.py
+++ app.py
@@ -7,3 +7,36 @@
 # the user will be able to play again or quit
 # the user will be able to see their score
 # the game interaction will happen in the terminal 
+
+import random
+import sys
+
+print("Welcome to Rock, Paper, Scissors!")
+print("Enter 'q' to quit the game")
+
+options = ["rock", "paper", "scissors"]
+score = 0
+
+while True:
+    user_choice = input("Enter rock, paper or scissors: ")
+    if user_choice == "q":
+        print("Goodbye!")
+        sys.exit()
+    if user_choice not in options:
+        print("Invalid choice. Please try again")
+        continue
+    computer_choice = random.choice(options)
+    print(f"Computer chose {computer_choice}")
+
+    if user_choice == computer_choice:
+        print("It's a draw!")
+    elif (user_choice == "rock" and computer_choice == "scissors") or (user_choice == "paper" and computer_choice == "rock") or (user_choice == "scissors" and computer_choice == "paper"):
+        print("You win!")
+        score += 1
+    else:
+        print("You lose!")
+    print(f"Score: {score}")
+    play_again = input("Do you want to play again? (y/n): ")
+    if play_again != "y":
+        print("Goodbye!")
+        sys.exit()
