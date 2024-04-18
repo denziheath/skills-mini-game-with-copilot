79db176363720a6a41401eaf233bbba321eb1e87
denziheath <118496561+denziheath@users.noreply.github.com>
2024-04-18 21:39:30 +0000

    added app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+# we are creating a game where the user has to choose one of three elements, rock, paper or scissors
+# the computer will also choose one of the three elements
+# if the user beats the computer, they get one point and vice versa
+# if the user and the computer choose the same element, it is a draw
+# paper beats rock, rock beats scissors, scissors beats paper
+# the user will be told if they won or lost
+# the user will be able to play again or quit
+# the user will be able to see their score
+# the game interaction will happen in the terminal 
